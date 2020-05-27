948317857dd3289c00f0530e2d6016f499937a0f
kristinadj <djkristinaxx@gmail.com>
2020-05-27 21:54:38 +0200

    BE project init

---
+++ cardiagnostic-be/src/main/java/sbz/cardiagnosticbe/CardiagnosticBeApplication.java
@@ -0,0 +1,13 @@
+package sbz.cardiagnosticbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardiagnosticBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardiagnosticBeApplication.class, args);
+	}
+
+}
---
+++ cardiagnostic-be/src/test/java/sbz/cardiagnosticbe/CardiagnosticBeApplicationTests.java
@@ -0,0 +1,13 @@
+package sbz.cardiagnosticbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardiagnosticBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
